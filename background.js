eea811873573426e810abda3002aa3df087cf176
cesine <cesine@yahoo.com>
2014-01-07 11:33:08 -0500

    adding a sandbox iframe

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
-    'bounds': {
-      'width': 400,
-      'height': 500
-    }
-  });
+chrome.app.runtime.onLaunched.addListener(function(launchData) {
+    var opt = {
+        width: 1000,
+        height: 700
+    };
+
+    chrome.app.window.create('sandbox.html', opt, function (win) {
+        win.launchData = launchData;
+    });
+
 });
