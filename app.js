dbdef268afd64870b73f6fff1c09617abc043592
cesine <cesine@yahoo.com>
2014-01-07 16:05:05 -0500

    passing messages without helper

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-GlobalPostMessage.request({
-    key: 'xhr-file',
-    params: {filename: 'package.json'},
-    success: function(data) {
-        console.log("[app.js] Post Message replied to xhr-file: \n" + data);
-    }
-});
+window.addEventListener('message', function(e) {
+	var data = e.data,
+		key = data.key;
+
+	if (data.deffered && data.result) {
+		console.log("[app.js] Post Message replied to : " + key + "\n" , data);
+		// data.success(data.result)
+	} else {
+		console.warn("[app.js] Post Message replied with no result to : " + key + "\n" , e);
+	}
+
+}, false);
+
+// window.top.postMessage({key: "testing"}, '*');
